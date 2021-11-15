e8143b50b2308f53de2153d7ad38512ae8e7f00d
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-15 16:39:29 +0000

    :rocket: fetch quotes randomly

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,42 @@ import { useEffect, useState } from "react";
 
 const App = () => {
   const [color, setColor] = useState("");
+  const [data, setData] = useState<any>();
+  const HOME_URL = "https://fetahokey.github.io/random-quote-machine/";
+
+  const updateQuote = async () => {
+    /*
+    author: "Samuel Taylor Coleridge"
+    authorSlug: "samuel-taylor-coleridge"
+    content: "Love is flower like; Friendship is like a sheltering tree."
+    dateAdded: "2020-02-27"
+    dateModified: "2020-02-27"
+    length: 58
+    tags: ['friendship']
+    _id: "LpBt1VcHO"
+    */
+    try {
+      const response = await fetch("https://api.quotable.io/random");
+      const { statusCode, statusMessage, ...data } = await response.json();
+      if (!response.ok) throw new Error(`${statusCode} ${statusMessage}`);
+      console.log(data);
+      setData(data);
+      setColor(randomMC.getColor());
+    } catch (error) {
+      // If the API request failed, log the error to console and update state
+      // so that the error will be reflected in the UI.
+      console.error(error);
+      setData({ content: "Opps... Something went wrong" });
+    }
+  };
 
   useEffect(() => {
     let effect = true;
-    setColor(randomMC.getColor());
+
+    updateQuote();
+
     return () => {
+      // eslint-disable-next-line
       effect = false;
     };
   }, []);
@@ -20,25 +51,35 @@ const App = () => {
     <Box className="Layout" bgcolor={color}>
       <Box id="quote-box" className="QuoteBox" component={Paper}>
         <Box pb={10}>
-          <IconButton id="new-quote" aria-label="fingerprint" color="secondary">
+          <IconButton
+            id="new-quote"
+            aria-label="fingerprint"
+            color="secondary"
+            onClick={updateQuote}
+          >
             <Loop />
           </IconButton>
         </Box>
         <Box id="text">
           <Box color={color}>
-            <FormatQuote fontSize="medium" /> In order to succeed, your desire
-            for success should be greater than your fear of failure.
+            <FormatQuote fontSize="medium" /> {data?.content}
           </Box>
         </Box>
-        <Box id="author">— Tony Hsieh</Box>
+        <Box id="author">— {data?.author}</Box>
 
-        <a id="tweet-quote" href="twitter.com/intent/tweet">
+        <a
+          id="tweet-quote"
+          href={`https://twitter.com/intent/tweet?hashtags=quotes,fetahokey&related=fatahokey&text=${data?.content} \n ${HOME_URL}`}
+        >
           <IconButton aria-label="twitter" color="primary">
             <Twitter />
           </IconButton>
         </a>
 
-        <a id="facebook-quote" href="facebook.com">
+        <a
+          id="facebook-quote"
+          href={`https://www.facebook.com/sharer/sharer.php?u=${HOME_URL}`}
+        >
           <IconButton aria-label="facebook" color="primary">
             <Facebook />
           </IconButton>
